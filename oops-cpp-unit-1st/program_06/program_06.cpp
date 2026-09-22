14fe8cb1e6ec7edc31594099b35ecc45bc4d1b1e
shreyadhurandhar <shreyadhurandhar2@gmail.com>
2026-09-22 11:42:20 +0530

    Add files via upload

---
+++ oops-cpp-unit-1st/program_06/program_06.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Demo {
+public:
+    Demo() {
+        cout << "Constructor called\n";
+    }
+
+    ~Demo() {
+        cout << "Destructor called\n";
+    }
+};
+
+int main() {
+    Demo d;
+
+    return 0;
+}
